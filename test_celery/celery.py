c90ed322d45138b2a75f1e167491e5122597580f
TonyWangBit <plantpark.net@gmail.com>
2018-12-19 20:01:36 +0800

    fix AttributeError error

--- test_celery/celery.py
+++ test_celery/celery.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import
 from celery import Celery
-app = Celery('test_celery',broker='amqp://admin:mypass@10.211.55.12:5672',backend='rpc://',include=['test_celery.tasks'])
+app = Celery('test_celery',broker='amqp://admin:mypass@rabbit:5672',backend='rpc://',include=['test_celery.tasks'])
 
